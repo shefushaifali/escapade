7374cd16b7d167aa217214986b87997b60efd5be
shefushaifali <shaifalitayal7@gmail.com>
2015-07-13 13:31:46 +0530

    first pages

---
+++ page_1.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+	<title>
+		escapade
+	</title>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Courgette|Playball|Oswald' rel="stylesheet" type="text/css" >
+  <style type="text/css">
+	.top_box
+	{
+	height: 570px;
+  width: 102%;
+  background-color: red;
+  margin-left: -9px;
+  margin-top: 38px;
+  background-image: url;
+  margin-bottom: -33px;
+}
+.bottom_box
+{
+	  height: 365px;
+  width: 102%;
+  background-color: yellow;
+  margin-left: -7px;
+}
+  .escapade
+  {
+  	height: 166px;
+  background-color: pink;
+  width: 515px;
+  margin-top: 150px;
+  position: fixed;
+  margin-left: 322px;
+  font-family: Playball;
+  font-size: 137px;
+  padding-left: 28px;
+
+
+  }
+  .login{
+  		height: 45px;
+  background-color: chocolate;
+  width: 100px;
+  padding-left: 13px;
+  margin-left: 36px;
+  margin-top: 32px;
+  border-radius: 18px;
+  padding-top: 2px;
+
+
+  }
+  .about{
+      height: 45px;
+  background-color: bisque;
+  width: 100px;
+  margin-left: 900px;
+  margin-top: -44px;
+  padding-left: 13px;
+  border-radius: 18px;
+  padding-top: 2px;
+  	 
+ }
+ .sign{
+  height: 45px;
+  background-color: rgb(160, 31, 31);
+  width: 100px;
+  margin-left: 557px;
+  margin-top: -43px;
+  border-radius: 18px;
+  padding-top: 2px;
+ 	  
+}
+ .fb{
+ 	    height: 45px;
+  background-color: rgba(87, 130, 166, 0.74);
+  width: 43px;
+  margin-left: 730px;
+  margin-top: 7px;
+  border-radius: 23px;
+
+ }
+.google{
+  height: 45px;
+  background-color: rgba(87, 130, 166, 0.74);
+  width: 43px;
+  margin-left: 433px;
+  margin-top: -45px;
+  border-radius: 23px;
+ 	  
+ }
+.twitter{
+height: 45px;
+  background-color: rgba(87, 130, 166, 0.74);
+  width: 43px;
+  margin-left: 584px;
+  margin-top: -45px;
+  border-radius: 23px
+ 	  
+ }
+.adventure{
+ 	  height: 40px;
+  background-color: rgb(18, 35, 208);
+  width: 118px;
+  padding-left: 13px;
+  margin-left: 222px;
+  margin-top: 17px;
+  border-radius: 18px;
+  padding-top: 2px;
+ }
+.destination{
+ 	  height: 40px;
+  background-color: rgb(92, 92, 255);
+  padding-left: 13px;
+  margin-left: 810px;
+  margin-top: -43px;
+  border-radius: 18px;
+  padding-top: 2px;
+
+ }
+
+
+</style>
+</head>
+<body>
+	<div class="top_box">
+		<div class="escapade">
+			Escapade</div>
+    </div>		
+     <div class="bottom_box">
+     	<div class="login">Login</div>
+     	<div class="about">About Us</div>
+     	<div class="sign">Sign Up</div>
+     	<div class="fb"></div>
+     	<div class="twitter"></div>
+     	<div class="google"></div>
+	    <div class="adventure">ADVENTURE</div>
+	    <div class="destination">DESTINATION</div>
+	</div>
+
+
+	</body>
+	</html>
